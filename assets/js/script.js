33ae5a1abd46a2718f8b8647b5e5e94b1c706c68
TheoAchmad <theosmk20@gmail.com>
2025-03-21 13:42:38 +0700

    fix all

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,16 @@
 // tambah class aktif
-const Navigasi = document.querySelector('.navigasi');
+const navnav = document.querySelector('.navigasi');
 
 // ketika hamburger di klik
 document.querySelector('#hamburger').onclick = () => {
-    Navigasi.classList.toggle('active');
+    navnav.classList.toggle('active');
 };
+
+// klik dimana saja untuk menghilangkan side bar
+const menu = document.querySelector('#hamburger');
+
+document.addEventListener('click', function(e) {
+    if(!menu.contains(e.target) && !navnav.contains(e.target)) {
+        navnav.classList.remove('active');
+    }
+})
